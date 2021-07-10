4393bad7c3db91042190859fdaf99461e20f312e
ArilessTir <ariless.tir@gmail.com>
2021-07-10 20:29:11 +0200

    add form & axios request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  const name = "ariless";
+const App = () => {
+  const [data, setData] = useState({
+    Pclass: "",
+    Sex: "",
+    Age: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(data);
+    axios
+      .post("http://localhost/predict", data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (key, value) => {
+    setData((obj) => ({
+      ...obj,
+      [key]: value,
+    }));
+  };
+
   return (
     <div className="App">
-      <h1> Hello titanic it is {name}</h1>
+      <form>
+        <input
+          placeholder="Pclass"
+          onChange={(e) => {
+            handleChange("Pclass", e.target.value);
+          }}
+        />
+        <input
+          placeholder="Sex"
+          onChange={(e) => {
+            handleChange("Sex", e.target.value);
+          }}
+        />
+        <input
+          placeholder="Age"
+          onChange={(e) => {
+            handleChange("Age", e.target.value);
+          }}
+        />
+        <input type="submit" value="Valider" onClick={handleSubmit} />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
