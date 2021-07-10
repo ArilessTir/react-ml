ad3b7a5ad17edcaba12ef3233f1939fc58ade49b
ArilessTir <ariless.tir@gmail.com>
2021-07-10 18:26:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  const name = "ariless";
+  return (
+    <div className="App">
+      <h1> Hello titanic it is {name}</h1>
+    </div>
+  );
+}
+
+export default App;
